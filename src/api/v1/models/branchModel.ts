25f90c8a282379e7b5e33565d62f81cb8e888e15
jasleen <jasleenkaurkalsi03@gmail.com>
2025-03-19 11:15:35 -0500

    docs: Add OpenAPI schema for Branch model

--- src/api/v1/models/branchModel.ts
+++ src/api/v1/models/branchModel.ts
@@ -1,8 +1,26 @@
-
-export interface Branch {
-    id: string;        
-    name: string;
-    address: string;
-    phone: string;
-  }
-  
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Branch:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Unique identifier for the branch
+ *         name:
+ *           type: string
+ *           description: Name of the branch
+ *         address:
+ *           type: string
+ *           description: Address of the branch
+ *         phone:
+ *           type: string
+ *           description: Contact phone number of the branch
+ */
+export type Branch = {
+  id: string;
+  name: string;
+  address: string;
+  phone: string;
+};
